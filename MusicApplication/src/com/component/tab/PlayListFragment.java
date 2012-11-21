09cade8937e42b1bc0eab6e8b7ae259191788680
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-22 01:27:32 +0800

    fragment

---
+++ MusicApplication/src/com/component/tab/PlayListFragment.java
@@ -0,0 +1,62 @@
+package com.component.tab;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockListFragment;
+import com.actionbarsherlock.app.ActionBar.Tab;
+
+public class PlayListFragment extends SherlockListFragment implements ActionBar.TabListener{
+
+	String playlist[] = new String[]{
+	        "Top ",
+	        "Moi xem",
+	        "Che Linh",
+	        "Duy Khanh"
+	      };
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+		 /** Creating array adapter to set data in listview */
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_list_item_multiple_choice, playlist);
+ 
+        /** Setting the array adapter to the listview */
+        setListAdapter(adapter);
+ 
+		super.onViewCreated(view, savedInstanceState);
+	}
+	
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		
+		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		ft.add(android.R.id.content, this,"Playlist");
+        ft.attach(this);
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		ft.detach(this);
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
