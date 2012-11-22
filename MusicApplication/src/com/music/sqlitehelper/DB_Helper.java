63134bfa37a8fef16237651d346cf3e97bed615f
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-22 19:53:31 +0800

    add helper for android.

---
+++ MusicApplication/src/com/music/sqlitehelper/DB_Helper.java
@@ -0,0 +1,48 @@
+package com.music.sqlitehelper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.music.model.MediaFile;
+
+public class DB_Helper extends SQLiteOpenHelper {
+	
+	Context contextObj;
+	
+	static final String TAG = "DB_Helper";
+	
+	static final String DB_NAME = "database.db";
+	static final int DB_VERSION = 1;
+	
+	public DB_Helper(Context context){
+		super(context,DB_NAME,null,DB_VERSION);
+		this.contextObj = context; 
+		Log.d(TAG, "Constructor...");
+	}
+	
+	
+	//Called Once when DB is created.
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		Log.d(TAG,"Start on Create Table");
+		Log.d(TAG, "End on Create Table");
+	}
+
+	//Called whenever oldVersion != newVersion.
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		Log.d(TAG,"Start On Upgrate");
+		
+		String sql_mediafile = "drop table if exists " + MediaFile.Table;
+		db.execSQL(sql_mediafile);
+		
+		Log.d(TAG, "End On Upgrate ");
+		onCreate(db);
+	}
+
+}
