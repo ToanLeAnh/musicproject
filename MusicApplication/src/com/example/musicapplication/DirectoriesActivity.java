ca71cef60785aa37a733d84bc6b29c5a79cecab5
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-23 00:15:14 +0800

    support valid folder

--- MusicApplication/src/com/example/musicapplication/DirectoriesActivity.java
+++ MusicApplication/src/com/example/musicapplication/DirectoriesActivity.java
@@ -10,52 +10,62 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DirectoriesActivity extends ListActivity  {
-
+	
+	private String[] unShowedFolder = {"/acct", "/cache", "/config", "/system","/sys","/sbin","/root","/etc"};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_list_directories);
 		
-		Log.i("tro","khong hieu chuyen gi");
-		
-		List<String> lststr = showDirectory();
-		
-		Log.i("Nghia","Chao the gioi");
+		List<String> lststr = showDirectory("/");
 		
 		ArrayAdapter<String> a = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice , lststr); 
-		
-		Log.i("nghia","sai o dau");
 		
 		setListAdapter(a);
 	}
 
-	/*
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_list_directories, menu);
-		return true;
-	}
-	*/
+	private List<String> showDirectory(String pathInfo){
 	
-	private List<String> showDirectory(){
-	
-		File rootDir = new File("/");
+		File rootDir = new File(pathInfo);
 		List<String> lststr = new ArrayList<String>();
 		
 		if (rootDir != null){
 			File[] dirArray = rootDir.listFiles();
 			for (File folder : dirArray){
 				if ((folder.isDirectory() == true) && (folder.isHidden()==false)){
-					lststr.add(folder.getPath().toString());
-					Log.i("Nghia oi","khong hieu");
+					if (isValidPath(folder.getPath().toString())){
+						
+						lststr.add(folder.getName().toString());
+					}
 				}
 			}
 		}
 		return lststr;
 	}
-
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+	
+	private boolean isValidPath(String pathInfo){	
+		
+		Log.i("Le Huu Nghia",pathInfo);
+		
+		for(String s : unShowedFolder){
+			Log.i("Bau oi",s);
+			if (s.equals(pathInfo)){
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
