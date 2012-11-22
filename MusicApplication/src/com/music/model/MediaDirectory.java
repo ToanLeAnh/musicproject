8471a7cb68ee419fe7b70f521f7b01269db7a718
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-22 18:28:57 +0800

    add  get ,set

---
+++ MusicApplication/src/com/music/model/MediaDirectory.java
@@ -0,0 +1,15 @@
+package com.music.model;
+
+public class MediaDirectory {
+	public final static String directory = "DIRECTORY";
+	
+	private String path;
+
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+}
