8e16615c90f2f70d8bcf8a5c22f10d306da4d63f
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-22 23:07:36 +0800

    add directory

--- MusicApplication/src/com/example/musicapplication/DirectoriesActivity.java
+++ MusicApplication/src/com/example/musicapplication/DirectoriesActivity.java
@@ -1,24 +1,61 @@
 package com.example.musicapplication;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DirectoriesActivity extends Activity {
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+public class DirectoriesActivity extends ListActivity  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_list_directories);
+		//setContentView(R.layout.activity_list_directories);
 		
+		Log.i("tro","khong hieu chuyen gi");
 		
+		List<String> lststr = showDirectory();
+		
+		Log.i("Nghia","Chao the gioi");
+		
+		ArrayAdapter<String> a = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice , lststr); 
+		
+		Log.i("nghia","sai o dau");
+		
+		setListAdapter(a);
 	}
 
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_list_directories, menu);
 		return true;
+	}
+	*/
+	
+	private List<String> showDirectory(){
+	
+		File rootDir = new File("/");
+		List<String> lststr = new ArrayList<String>();
+		
+		if (rootDir != null){
+			File[] dirArray = rootDir.listFiles();
+			for (File folder : dirArray){
+				if ((folder.isDirectory() == true) && (folder.isHidden()==false)){
+					lststr.add(folder.getPath().toString());
+					Log.i("Nghia oi","khong hieu");
+				}
+			}
+		}
+		return lststr;
 	}
 
 }
