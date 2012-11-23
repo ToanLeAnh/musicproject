0fe96f7c300fba697572478a52323529e08a6b9b
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-23 10:56:22 +0800

    add more info for dir model

--- MusicApplication/src/com/music/model/MediaDirectory.java
+++ MusicApplication/src/com/music/model/MediaDirectory.java
@@ -3,10 +3,28 @@ package com.music.model;
 public class MediaDirectory {
 	
 	public final static String Table = "tbl_directories";
-	
 	public final static String Col_DIRECTORY = "DIRECTORY";
 	
 	private String path;
+	private String name;
+	private String isChecked = "N";
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getIsChecked() {
+		return isChecked;
+	}
+	public void setIsChecked(String isChecked) {
+		this.isChecked = isChecked;
+	}
+	
 	
 	public String getPath() {
 		return path;
