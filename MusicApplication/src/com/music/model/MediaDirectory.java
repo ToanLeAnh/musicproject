c038f977a2d8676d96904b58b4899cea57889200
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-23 21:56:23 +0800

    Fix Name

--- MusicApplication/src/com/music/model/MediaDirectory.java
+++ MusicApplication/src/com/music/model/MediaDirectory.java
@@ -3,8 +3,8 @@ package com.music.model;
 public class MediaDirectory {
 	
 	public final static String Table = "tbl_directories";
-	public final static String Col_DIRECTORY = "DIRECTORY_NAME";
-	public final static String Col_DiRECTORY = "DIRECTORY_PATH";
+	public final static String Col_DIRECTORY_NAME = "DIRECTORY_NAME";
+	public final static String Col_DiRECTORY_PATH = "DIRECTORY_PATH";
 	
 	private String path;
 	private String name;
