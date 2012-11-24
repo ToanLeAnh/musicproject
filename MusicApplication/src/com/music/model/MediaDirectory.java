67a973b5cc18534bd8658cc0bae1a5457f9f7ad4
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-24 14:45:46 +0800

    modify Model

--- MusicApplication/src/com/music/model/MediaDirectory.java
+++ MusicApplication/src/com/music/model/MediaDirectory.java
@@ -2,10 +2,6 @@ package com.music.model;
 
 public class MediaDirectory {
 	
-	public final static String Table = "tbl_directories";
-	public final static String Col_DIRECTORY_NAME = "DIRECTORY_NAME";
-	public final static String Col_DiRECTORY_PATH = "DIRECTORY_PATH";
-	
 	private String path;
 	private String name;
 	private String isChecked = "N";
