d891df0dd3300052ffacce040b4c37241a63d7de
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-24 19:12:22 +0800

    add new code

---
+++ MusicApplication/src/com/music/objectmapping/Datasource_mediafile.java
@@ -0,0 +1,16 @@
+package com.music.objectmapping;
+
+public class Datasource_mediafile {
+	public final static String Table = "tbl_media";
+	
+	public final static String Col_PATH =	"PATH";
+	public final static String Col_DURATION = "DURATION";
+	public final static String Col_TYPE = "TYPE";
+	public final static String Col_PICTURE = "PICTURE";
+	public final static String Col_TITLE = "TITLE";
+	public final static String Col_SIZE =	"SIZE";
+	public final static String Col_ARTIST = "ARTIST";
+    public final static String Col_GENER = "GENER";
+    public final static String Col_ALBUM = "ALBUM";
+							
+}
