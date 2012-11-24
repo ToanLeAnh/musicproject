dafdf698d71c9bcb70dcd200999a43e8b2e46efd
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-24 19:18:44 +0800

    Add code

--- MusicApplication/src/com/music/model/MediaFile.java
+++ MusicApplication/src/com/music/model/MediaFile.java
@@ -4,18 +4,6 @@ import android.provider.MediaStore;
 
 public class MediaFile {
 	
-	public final static String Table = "tbl_media";
-	
-	public final static String Col_PATH =	"PATH";
-	public final static String Col_DURATION = "DURATION";
-	public final static String Col_TYPE = "TYPE";
-	public final static String Col_PICTURE = "PICTURE";
-	public final static String Col_TITLE = "TITLE";
-	public final static String Col_SIZE =	"SIZE";
-	public final static String Col_ARTIST = "ARTIST";
-    public final static String Col_GENER = "GENER";
-    public final static String Col_ALBUM = "ALBUM";
-							
 	private String path;
 	private String duration;
 	private String type;
@@ -24,7 +12,6 @@ public class MediaFile {
 	private String size;
 	private String artist;
 	private String album;
-	
 	
 	public String getPath() {
 		return path;
