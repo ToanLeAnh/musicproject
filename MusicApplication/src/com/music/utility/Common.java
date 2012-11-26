59884326c7fa19593352fc0207eb733eac2476b9
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-27 02:51:15 +0800

    modify code

--- MusicApplication/src/com/music/utility/Common.java
+++ MusicApplication/src/com/music/utility/Common.java
@@ -1,9 +1,13 @@
 package com.music.utility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Common {
 
 	public static String checkData(String data){
 		return (data == null) ? null : data; 
 	}
+	
 	
 }
