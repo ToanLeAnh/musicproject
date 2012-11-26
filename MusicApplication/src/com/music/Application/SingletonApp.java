5fc447ffc9a9a8d925d6c16b2dd4ac1d153484dc
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-27 02:50:02 +0800

    modify

--- MusicApplication/src/com/music/Application/SingletonApp.java
+++ MusicApplication/src/com/music/Application/SingletonApp.java
@@ -12,7 +12,7 @@ import com.music.model.MediaFile;
 import com.music.objectmapping.Datasource_mediafile;
 
 public class SingletonApp {
-	
+	private static List<MediaFile> list_combine = null;
 	private static List<MediaFile> list_media = null;
 	private static List<MediaFile> list_media_folder = null;
 	private static MediaAlbums dic_albums=null;
@@ -23,6 +23,7 @@ public class SingletonApp {
 	
 	
 	public static void init(Context contextObj){
+		list_combine = new ArrayList<MediaFile>();
 		list_media = new ArrayList<MediaFile>();
 		list_media_folder = new ArrayList<MediaFile>();
 		dic_albums = new MediaAlbums();
@@ -33,20 +34,21 @@ public class SingletonApp {
 		//Log.d(TAG,String.valueOf(list_media.size()));
 		if (list_media.size() == 0){
 			Datasource_mediafile transfer_mediafile = new Datasource_mediafile(context);
-			list_media = transfer_mediafile.getAllMediaFromExternalDevices();		
+			list_media = transfer_mediafile.getAllMediaFromExternalDevices();
+			list_combine = list_media;
 		}
 	}
 	
 	public static void getListMediaFileFromFolders(){
 		try{
-			lock.lock();
+			//lock.lock();
 			if (list_media_folder.size() == 0){
 				Datasource_mediafile transfer_mediafile = new Datasource_mediafile();
 				list_media_folder = transfer_mediafile.getAllMediaFromSpecifiedFolders();
 			}
 		}
 		finally{
-			lock.unlock();
+			//lock.unlock();
 		}
 	}
 
@@ -55,18 +57,38 @@ public class SingletonApp {
 			public void run() {
 				getListMediaFileFromFolders();
 				for (MediaFile a : list_media_folder){
-					list_media.add(a);
+					/*if (!list_combine.contains(a.getc)){
+						list_combine.add(a);
+					}
+					*/
+					int push = -1;
+					MediaFile c = new MediaFile();;
+					c= a;
+					for (MediaFile b : list_media){
+						Log.d("Nghia",a.getPath());
+						Log.d("Nghia",b.getPath());
+						if (a.equals(b)){
+							push = 0 ;
+							break;
+						}
+					}
+					
+					if ((c != null) && (push == -1)) {
+						Log.d("kkk",String.valueOf(push));
+						list_combine.add(c);
+					}
 				}
 			};
 		}.start();
 	}
 	
 	public static List<MediaFile> getList_media() {
-		return list_media;
+		return list_combine;
 	}
 	
 	public static List<MediaFile> getList_media_folder(){
 		return list_media_folder;
 	}
+	
 	
 }
