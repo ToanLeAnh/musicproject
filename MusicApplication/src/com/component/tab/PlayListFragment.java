b06ee7b29d044853f0f4f756faeebeff04eefc3f
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-28 17:17:40 +0800

    modify code

--- MusicApplication/src/com/component/tab/PlayListFragment.java
+++ MusicApplication/src/com/component/tab/PlayListFragment.java
@@ -1,5 +1,6 @@
 package com.component.tab;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
@@ -9,24 +10,27 @@ import android.widget.ListView;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockListFragment;
 import com.actionbarsherlock.app.ActionBar.Tab;
+import com.music.model.MediaPlayList;
+import com.music.objectmapping.DataSource_playlist;
+import com.music.utility.playlist_format_view;
 
 public class PlayListFragment extends SherlockListFragment implements ActionBar.TabListener{
 
-	String playlist[] = new String[]{
-	        "Top ",
-	        "Moi xem",
-	        "Che Linh",
-	        "Duy Khanh"
-	      };
+	protected final String TAG = "PlayListFragment";
+	Context context;
 	
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		
 		 /** Creating array adapter to set data in listview */
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_list_item_multiple_choice, playlist);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_list_item_multiple_choice, playlist);
  
         /** Setting the array adapter to the listview */
+		context = view.getContext();
+		DataSource_playlist dataPlaylist = new DataSource_playlist(context);
+		dataPlaylist.getAllThePlayPlistFromDB();
+        ArrayAdapter<MediaPlayList> adapter = new playlist_format_view(context,com.example.musicapplication.R.layout.playplist_view,dataPlaylist.getPlayplist());
         setListAdapter(adapter);
  
 		super.onViewCreated(view, savedInstanceState);
