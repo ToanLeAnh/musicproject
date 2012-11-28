04808c8d5cbe63007c08ac2e411bd24380495162
LeHuuNghia <lehuunghia02@yahoo.com>
2012-11-28 12:46:16 +0800

    modify code

--- MusicApplication/src/com/music/objectmapping/DataSource_groupmedia.java
+++ MusicApplication/src/com/music/objectmapping/DataSource_groupmedia.java
@@ -11,45 +11,85 @@ import com.music.model.MediaFile;
 
 public class DataSource_groupmedia {
 	
-	List<HashMap<String,List<MediaFile>>> group; 
+	//This is support for group album
+	List<HashMap<String,ArrayList<MediaFile>>> group;
+	
+	//
+	List<HashMap<String,ArrayList<MediaFile>>> groupArtist;
+	
 	Object lock = new Object();
+	Object lock2 = new Object();
+	private String unknow = "Unknow";
+	
+	
 	protected String TAG="DataSource_groupmedia";
 	
-	public List<HashMap<String, List<MediaFile>>> getGroup() {
+	public List<HashMap<String, ArrayList<MediaFile>>> getGroup() {
 		return group;
 	}
 
+	public List<HashMap<String, ArrayList<MediaFile>>> getArtist(){
+		return groupArtist;
+	}
 	
 	public DataSource_groupmedia(){
-		group = new ArrayList<HashMap<String,List<MediaFile>>>();
-		Log.d(TAG,"Init Group");
+		group = new ArrayList<HashMap<String,ArrayList<MediaFile>>>();
+		groupArtist = new ArrayList<HashMap<String,ArrayList<MediaFile>>>();
 	}
 	
 	public void add_MediaFileToAlbums(MediaFile mediaFile){
-		Log.d(TAG,"Add here");
+		String nameAlbums = mediaFile.getAlbum();
+		int i = 0 ;
+		int isFound =1;
+		
+		if (nameAlbums == ""){
+			nameAlbums = unknow;
+		}
+		
 		synchronized(lock){
-			Log.d(TAG,"Inside Method");
-			String nameAlbums = mediaFile.getAlbum();
-			
-			int i = 0 ;
-			int isFound =1;
 			for(i = 0;i < group.size() ; i ++){
 				isFound = 1;
 				if (group.get(i).containsKey(nameAlbums)){
 					group.get(i).get(nameAlbums).add(mediaFile);
 					isFound = 0;
-					Log.d(TAG,"Add new MediaFile");
 					break;
 				}
 			}
 			
 			if (isFound == 1){
-				HashMap<String, List<MediaFile>> new1= new HashMap<String, List<MediaFile>>();
+				HashMap<String, ArrayList<MediaFile>> new1= new HashMap<String, ArrayList<MediaFile>>();
 				ArrayList<MediaFile> a = new ArrayList<MediaFile>();
 				a.add(mediaFile);
 				new1.put(nameAlbums,a);
 				group.add(new1);
-				Log.d(TAG,"Add new MediaFile");
+			}
+		}
+	}
+	
+	public void add_MediaFileToArtist(MediaFile mediaFile){
+		String artist = mediaFile.getArtist();
+		int i = 0 ;
+		int isFound =1;
+		if (artist == ""){
+			artist = unknow;
+		}
+		
+		synchronized(lock2){
+			for(i = 0;i < groupArtist.size() ; i ++){
+				isFound = 1;
+				if (groupArtist.get(i).containsKey(artist)){
+					groupArtist.get(i).get(artist).add(mediaFile);
+					isFound = 0;
+					break;
+				}
+			}
+			
+			if (isFound == 1){
+				HashMap<String, ArrayList<MediaFile>> new1= new HashMap<String, ArrayList<MediaFile>>();
+				ArrayList<MediaFile> a = new ArrayList<MediaFile>();
+				a.add(mediaFile);
+				new1.put(artist,a);
+				groupArtist.add(new1);
 			}
 		}
 	}
